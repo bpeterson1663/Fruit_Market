2f5be26c5fcc4b0975af220f81cb3cb1f4e19e4a
bpeterson1663 <bpeterson1663@gmail.com>
2016-03-01 22:36:05 -0600

    Fixed functionality so that when the button is clicked it subtracts the current price from the total

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var fruitsArray = [];
 var fruitPrice;
 // set the total cash
 var totalCash = 100;
-
-
 //build object via constructor Fruit
 function Fruit (name, price) {
   this.name = name;
@@ -14,34 +12,34 @@ function Fruit (name, price) {
 }
 
 //create new fruit objects
-var apple = new Fruit ("Apple", 5.00);
-var orange = new Fruit ("Orange", 5.00);
-var banana = new Fruit ("Banana", 5.00);
+var apple = new Fruit ("Apple", 0);
+var orange = new Fruit ("Orange", 0);
+var banana = new Fruit ("Banana", 0);
 //var grape = new Fruit ("Grapes", 5.00);
-var pear = new Fruit ("Pear", 5.00);
+var pear = new Fruit ("Pear", 0);
 //var watermelon = new Fruit ("Watermelon", 5.00);
 
 //jquery document ready
 $(document).ready(function(){
-  $('.fruits').append('<div class="cash">Total Cash: $' + totalCash + '</div>');
+  $('.fruits').append('<div class="cash">Total Cash: $<span id="totalCash">' + totalCash + '</span></div>');
   for(var i = 0; i < fruitsArray.length; i++) {
     //set the fruit price and append to the DOM
-    //setInitial(fruitsArray[i]);
+    setInitial(fruitsArray[i]);
     appendDOM(fruitsArray[i]);
   }
- $('.button').on('click', spendMoney);
+ $('.fruits').on('click','button', spendMoney);
 });
 
-function spendMoney () {
+function spendMoney(){
   var price = $(this).data('price');
   totalCash -= price;
-  console.log(totalCash);
-
+  console.log(price);
+  $('#totalCash').text(totalCash);
 }
 
 //set the initial price for each fruit
 function setInitial(object){
-  object.price += randomNumber(-0.5, 0.5);
+  object.price += randomNumber(0.5, 9.99);
 
 }
 //initial append to the DOM
@@ -51,41 +49,38 @@ function appendDOM(object){
 
   $el.append('<p class="' + object.name + '">Fruit Name: ' + object.name + '</p>');
   $el.append('<p class="' + object.name + '-price">Fruit Price: $' + object.price + '</p>');
-  $el.append('<button class="button"> Buy </button>');
+  $el.append('<button id="' + object.name + '"> Buy </button>');
   //$('.button').data('price', object.price);
   //console.log(object);
   // store the data to the button
 
 }
 // set up the function to generate random numbers for each fruit price
+//var fruitPrice = randomNumber(0.5, 9.99);
 function setPrice() {
   for(var i = 0; i < fruitsArray.length; i++){
-    fruitPrice = fruitsArray[i].price;
-    fruitPrice = randomNumber(0.5, 9.99);
-    fruitPrice += randomNumber(-0.5, 0.5);
-    if(fruitPrice < 0.5){
-      fruitPrice += randomNumber(0, 0.5);
-    } else if(fruitPrice > 9.99){
-      fruitPrice -= randomNumber(0, 0.5);
-    } else {
-      fruitPrice += randomNumber(-0.5, 0.5);
-    }
-    var $el = $('.fruits').children().last();
-    var $button = $el.parent().find('button');
-    //var price = parseFloat(object.price);
-    $button.data('fruit-price', fruitPrice);
-    //fruitPrice = fruitPrice.toFixed(2);//to fix the number into two decimal places
 
-//append the new price for the fruit
-    $('.eachfruit').find('.' + fruitsArray[i].name + '-price').text("Fruit Price: $" + fruitPrice);
-    fruitsArray[i].price = fruitPrice;
+    fruitsArray[i].price += randomNumber(-0.5, 0.5);
+    while(fruitsArray[i].price < 0.5){
+      fruitsArray[i].price += randomNumber(0,0.5);
+    }
+    while(fruitsArray[i].price > 9.99){
+      fruitsArray[i].price -= randomNumber(0,0.5);
+    }
+
+    $('.eachfruit').find('.' + fruitsArray[i].name + '-price').text("Fruit Price: $" + fruitsArray[i].price.toFixed(2));
+    $('#'+fruitsArray[i].name+'').data('price', fruitsArray[i].price.toFixed(2));
+
     //console.log(fruitsArray);
   }
 
 }
 //random number generator
-function randomNumber(min, max) {
-  return Math.floor((Math.random() * (1 + max - min) + min) * 100) / 100;
- }
+function randomNumber(min, max){
+  return Math.random() * (1 + max - min) + min*100/100;
+}
+
+var test = randomNumber(0.5,9.99);
+console.log(test);
 //built in function to change the price every 15 seconds
 setInterval(setPrice, 3000);
